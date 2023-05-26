2c356ee5e62cd6919809309eca9141c04b55f532
James <james.hinton@live.co.uk>
2023-05-26 17:01:23 +0100

    feat: upload shape and proj file

--- src/components/ShapefileLoader/index.jsx
+++ src/components/ShapefileLoader/index.jsx
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
-
+import React, { useRef } from "react";
+import proj4 from "proj4";
+import epsg from "epsg";
+// var wktParser = require("wkt-parser");
+import wktParser from "wkt-parser";
 const shapefile = require("shapefile");
 
+function getSRSFromWKT(wkt) {
+  console.log("WKT is: ", wkt);
+  let code = null;
+
+  console.log(wktParser(wkt));
+
+  return code;
+}
+
 const ShapefileLoader = () => {
+  const fileInputShp = useRef();
+  const fileInputPrj = useRef();
 
+  const handleShapefileUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
 
-  useEffect(() => {
     shapefile
-      .open("/RGN_DEC_1921_EW_BGC.shp")
+      .open(file.path)
       .then((source) =>
         source.read().then(function log(result) {
           if (result.done) return;
@@ -16,14 +32,45 @@ const ShapefileLoader = () => {
         })
       )
       .catch((error) => console.error(error.stack));
-  }, []);
+  };
 
+  const handlePrjfileUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const wkt = e.target.result;
+      console.log("WKT is: ", wkt);
+      const srsCode = getSRSFromWKT(wkt);
+      console.log(`EPSG code is: ${srsCode}`);
+    };
+    reader.readAsText(file);
+  };
 
   return (
-    <>
-      Hello Moto
-    </>
-  )
-}
+    <div>
+      <input
+        type="file"
+        ref={fileInputShp}
+        style={{ display: "none" }}
+        onChange={handleShapefileUpload}
+      />
+      <button onClick={() => fileInputShp.current.click()}>
+        Upload Shapefile
+      </button>
+
+      <input
+        type="file"
+        ref={fileInputPrj}
+        style={{ display: "none" }}
+        onChange={handlePrjfileUpload}
+      />
+      <button onClick={() => fileInputPrj.current.click()}>
+        Upload PRJ file
+      </button>
+    </div>
+  );
+};
 
 export default ShapefileLoader;
